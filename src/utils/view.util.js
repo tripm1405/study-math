a975931463ec3593f973672eef261a87144e217e
Phạm Minh Trí <tri.pham@idtek.com.vn>
2024-05-29 14:15:49 +0700

    init layout data

--- src/utils/view.util.js
+++ src/utils/view.util.js
@@ -1,7 +1,55 @@
+const menuItems = {
+  course: {
+    label: 'Khoá học',
+    link: '/courses',
+  },
+  'class': {
+    label: 'Khoá học',
+    link: '/courses',
+  },
+  lesson: {
+    label: 'Khoá học',
+    link: '/courses',
+  },
+  user: {
+    label: 'Khoá học',
+    link: '/courses',
+  },
+  block: {
+    label: 'Khoá học',
+    link: '/courses',
+  },
+};
+
+const currentUser = {
+  fullName: 'Full Name',
+  type: 'GiaoVien'
+}
+
 export default class ViewUtil {
-  static getOptions = (options) => ({
-    layout: 'layouts/main.layout.ejs',
-    data: {},
-    ...options,
-  })
+  static getOptions = (options, isManager = false) => {
+    const menu = isManager
+        ? [
+          menuItems.course,
+          menuItems.class,
+          menuItems.lesson,
+          menuItems.user,
+          menuItems.block,
+        ]
+        : [
+          menuItems.course,
+          menuItems.class,
+          menuItems.lesson,
+        ];
+
+    return {
+      layout: 'layouts/main.layout.ejs',
+      ...options,
+      data: {
+        menu: menu,
+        user: currentUser,
+        ...options.data,
+      },
+    };
+  }
 }
