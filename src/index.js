d6d4af93257258c7ad1c139a829e58bf4c8aea87
tripm1405 <tri.pm.1405@gmail.com>
2024-07-10 13:08:50 +0700

    test env

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import expressEjsLayouts from 'express-ejs-layouts';
 import cookieParser from 'cookie-parser';
-import { dirname } from 'path';
+import {dirname} from 'path';
 import {fileURLToPath} from 'url';
 import dotenv from "dotenv";
 import mongoose from "mongoose";
@@ -13,10 +13,9 @@ import ApiUtil from "#root/utils/api.util.js";
 dotenv.config();
 
 try {
-  await mongoose.connect(process.env.DB_SERVER);
-}
-catch {
-  console.log('connect database error');
+    await mongoose.connect(process.env.DB_SERVER);
+} catch {
+    console.log('connect database error');
 }
 
 
@@ -32,56 +31,57 @@ app.set('view engine', 'ejs');
 app.use(express.static(`${rootPath}/public`));
 app.use(expressEjsLayouts);
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(multer({ dest: `${rootPath}/uploads` }).any());
+app.use(express.urlencoded({extended: true}));
+app.use(multer({dest: `${rootPath}/uploads`}).any());
 app.use(cookieParser());
 
 app.use((req, res, next) => {
-  const {
-    method,
-    url,
-    body,
-    query,
-    files,
-  } = req;
+    const {
+        method,
+        url,
+        body,
+        query,
+        files,
+    } = req;
 
-  if (url === '/favicon.ico') {
+    if (url === '/favicon.ico') {
+        next();
+        return;
+    }
+
+    console.log({
+        method,
+        url,
+        body,
+        query,
+        files: files?.map(file => file?.originalname),
+    });
+
     next();
-    return;
-  }
-
-  console.log({
-    method,
-    url,
-    body,
-    query,
-    files: files?.map(file => file?.originalname),
-  });
-
-  next();
 });
 
 app.use(Router);
 
 app.use((err, req, res, next) => {
-  console.log(err);
-  if (err.name === 'ValidationError') {
+    console.log(err);
+    if (err.name === 'ValidationError') {
+        res.json(ApiUtil.JsonRes({
+            success: false,
+            errors: err?.errors?.content,
+        }));
+
+        return;
+    }
+
     res.json(ApiUtil.JsonRes({
-      success: false,
-      errors: err?.errors?.content,
+        success: false,
+        errors: 'Server error!',
     }));
-
-    return;
-  }
-
-  res.json(ApiUtil.JsonRes({
-    success: false,
-    errors: 'Server error!',
-  }));
 })
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+    console.log(`http://localhost:${PORT}`);
+    console.log(process.env.host);
 });
 
 export default app;
